51633196d2583ee60365745d777bb56bdff17a40
kl414 <kevinlin414@gmail.com>
2015-02-05 15:59:43 -0500

    Update Find_Minimum_in_Rotated_Sorted_Array.java

--- Find_Minimum_in_Rotated_Sorted_Array.java
+++ Find_Minimum_in_Rotated_Sorted_Array.java
@@ -2,6 +2,7 @@
  * when the next one is less than the current one
  * then it's the first item before rotation
  */
+/*
 public class Solution {
     public int findMin(int[] num) {
         if(num.length == 0) return -1;
@@ -14,5 +15,32 @@ public class Solution {
             }
         }
         return min;
+    }
+}
+*/
+/**
+ * could also use binary search to get O(logn)
+ */
+public class Solution {
+    public int findMin(int[] num) {
+        if(num.length == 0) return -1;
+        if(num.length == 1) return num[0];
+        
+        int lo = 0;
+        int hi = num.length - 1;
+        
+        while(lo <= hi){
+            
+            if(num[lo] <= num[hi])
+                return num[lo];
+                
+            int mid = (hi + lo) / 2;
+            if(num[mid] >= num[lo]){
+                lo = mid + 1;
+            }else{
+                hi = mid;
+            }
+        }
+        return num[lo];
     }
 }
