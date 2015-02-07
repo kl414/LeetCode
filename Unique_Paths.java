0659542f51bb96cd614c2697ac5de3ee2795c93e
kl414 <kevinlin414@gmail.com>
2015-02-07 17:23:45 -0500

    Unique Paths #62

---
+++ Unique_Paths.java
@@ -0,0 +1,38 @@
+/**
+ * Idea
+ * the path to m * n is the sume of (path m-1 * n) and (path m, n - 1)
+ * the following code is a better optimization which only use 1-D array instead of 2-D
+ * but the run-time should be the same as O(m*n)
+ */
+ /*
+public class Solution {
+    public int uniquePaths(int m, int n) {
+        int[] dp = new int[n];
+        for (int i = 0; i < n; i++) {
+            dp[i] = 1;
+        }
+        for (int i = 1; i < m; i++) {
+            for (int j = 1; j < n; j++) {
+                dp[j] += dp[j - 1];
+            }
+        }
+        return dp[n - 1];
+    }
+}
+*/
+
+//this is a mathematical solution(copied the code(d40a) just for extra knowledge lol)
+public class Solution {
+    public int uniquePaths(int m, int n) {
+        int N = n + m - 2;// how much steps we need to do
+        int k = m - 1; // number of steps that need to go down
+        double res = 1;
+        // here we calculate the total possible path number 
+        // Combination(N, k) = n! / (k!(n - k)!)
+        // reduce the numerator and denominator and get
+        // C = ( (n - k + 1) * (n - k + 2) * ... * n ) / k!
+        for (int i = 1; i <= k; i++)
+            res = res * (N - k + i) / i;
+        return (int)res;
+    }
+}
