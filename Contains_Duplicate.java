b2d0b9f50766148b6fb60712b937967889d2523f
kl414 <kevinlin414@gmail.com>
2015-12-01 22:07:18 -0500

    Update Contains_Duplicate.java

--- Contains_Duplicate.java
+++ Contains_Duplicate.java
@@ -1,1 +1,15 @@
-
+/**
+ * Use HashSet since it won't add duplicates
+ */
+public class Solution {
+    public boolean containsDuplicate(int[] nums) {
+        HashSet<Integer> result = new HashSet<Integer>();
+        for(int num: nums){
+            if(result.contains(num)){
+                return true;
+            }
+            result.add(num);
+        }
+        return false;
+    }
+}
