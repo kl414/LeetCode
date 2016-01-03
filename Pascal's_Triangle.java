5eff0959f71d8637947bf0ba9f00b83803be504e
kl414 <kevinlin414@gmail.com>
2016-01-02 22:47:01 -0500

    Pascal's Triangle #118

---
+++ Pascal's_Triangle.java
@@ -0,0 +1,43 @@
+/**
+ * Below is actually slower, but provides a new way of thinking
+ */
+public class Solution {
+    public List<List<Integer>> generate(int numRows) {
+        List<List<Integer>> allrows = new ArrayList<List<Integer>>();
+        ArrayList<Integer> row = new ArrayList<Integer>();
+        for(int i=0;i<numRows;i++)
+        {
+            row.add(0, 1);
+            for(int j=1;j<row.size()-1;j++)
+                row.set(j, row.get(j)+row.get(j+1));
+            allrows.add(new ArrayList<Integer>(row));
+        }
+        return allrows;
+        }
+}
+
+/**
+ * This is a more understandable and straight way
+ */
+/*
+public class Solution {
+    public List<List<Integer>> generate(int numRows) {
+        List<List<Integer>> triangle = new ArrayList<List<Integer>>();
+        if (numRows <=0){
+            return triangle;
+        }
+        for (int i=0; i<numRows; i++){
+            List<Integer> row =  new ArrayList<Integer>();
+            for (int j=0; j<i+1; j++){
+                if (j==0 || j==i){
+                    row.add(1);
+                } else {
+                    row.add(triangle.get(i-1).get(j-1)+triangle.get(i-1).get(j));
+                }
+            }
+            triangle.add(row);
+        }
+        return triangle;
+    }
+}
+*/
