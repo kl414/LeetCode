9816bd30162025f7917d7c0c0101bc7ec993b3a7
kl414 <kevinlin414@gmail.com>
2016-01-23 23:30:18 -0500

    Update Remove Linked_List_Elements.java

--- Remove Linked_List_Elements.java
+++ Remove Linked_List_Elements.java
@@ -1,5 +1,43 @@
-public ListNode removeElements(ListNode head, int val) {
-        if (head == null) return null;
-        head.next = removeElements(head.next, val);
-        return head.val == val ? head.next : head;
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) { val = x; }
+ * }
+ */
+public class Solution {
+    public ListNode removeElements(ListNode head, int val) {
+        //can also use ListNode fakeHead = new ListNode(-1); to avoid the checking
+        //or return head.val == val ? head.next : head; to avoid
+        while(head != null && head.val == val)
+            head = head.next;
+            
+        if(head == null) return null;
+        ListNode pre = head;
+        ListNode curr = head.next;
+        
+        while(curr != null){
+            if(curr.val == val){
+                pre.next = curr.next;
+            }else{
+                pre = curr;
+            }
+            curr = curr.next;
+        }
+        return head;
+    }
 }
+
+//recursive method
+/*
+public class Solution {
+    public ListNode removeElements(ListNode head, int val) {
+        if(head == null) return null;
+        ListNode next = removeElements(head.next, val);
+        if(head.val == val) return next;
+        head.next = next;
+        return head;
+    }
+}
+*/
