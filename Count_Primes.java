d3b251c7dc392068c6dc9e320e1466334183a9f2
kl414 <kevinlin414@gmail.com>
2016-01-30 20:35:06 -0500

    Count Primes #204

---
+++ Count_Primes.java
@@ -0,0 +1,20 @@
+/**
+ * notPrime array stores all the numbers that are multiple of founded prime numbers
+ */
+public class Solution {
+    public int countPrimes(int n) {
+        boolean[] notPrime = new boolean[n];
+        int count = 0;
+        
+        for(int i = 2; i < n; i++){
+            if(notPrime[i] != true){
+                count++;
+                for(int j = 2; i * j < n; j++){
+                    notPrime[i*j] = true;
+                }
+            }
+        }
+        
+        return count;
+    }
+}
