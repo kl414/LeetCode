4d0ba59fc5a326d1421f427283171a92aaee454f
kl414 <kevinlin414@gmail.com>
2016-01-31 18:43:34 -0500

    Update ZigZag_Conversion.java

--- ZigZag_Conversion.java
+++ ZigZag_Conversion.java
@@ -1,17 +1,25 @@
-public String convert(String s, int nRows) {
-    char[] c = s.toCharArray();
-    int len = c.length;
-    StringBuffer[] sb = new StringBuffer[nRows];
-    for (int i = 0; i < sb.length; i++) sb[i] = new StringBuffer();
-
-    int i = 0;
-    while (i < len) {
-        for (int idx = 0; idx < nRows && i < len; idx++) // vertically down
-            sb[idx].append(c[i++]);
-        for (int idx = nRows-2; idx >= 1 && i < len; idx--) // obliquely up
-            sb[idx].append(c[i++]);
+/**
+ * use the StringBuffer array to store each row's character
+ */
+public class Solution {
+    public String convert(String s, int numRows) {
+        int len = s.length();
+        StringBuffer[] sb = new StringBuffer[numRows];
+        for(int i = 0; i < numRows; i++) sb[i] = new StringBuffer();
+        
+        for(int i = 0; i < len;){
+            for(int j = 0; j < numRows && i < len; j++){
+                sb[j].append(s.charAt(i++)); // vertical
+            }
+            //there are numRows - 2 chars obliquely
+            for(int j = numRows - 2; j >= 1 && i < len; j--){
+                sb[j].append(s.charAt(i++)); // diagnal
+            }
+        }
+        
+        for(int i = 1; i < numRows; i++)
+            sb[0].append(sb[i]);
+            
+        return sb[0].toString();
     }
-    for (int idx = 1; idx < sb.length; idx++)
-        sb[0].append(sb[idx]);
-    return sb[0].toString();
 }
