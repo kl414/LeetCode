45038bf0678380482649133d084e2bf0c84198ed
kl414 <kevinlin414@gmail.com>
2016-01-30 20:22:35 -0500

    Update Valid_Palindrome.java

--- Valid_Palindrome.java
+++ Valid_Palindrome.java
@@ -1,26 +1,25 @@
 public class Solution {
     public boolean isPalindrome(String s) {
-        if (s.isEmpty()) {
-            return true;
-        }
-        int head = 0, tail = s.length() - 1;
-        char cHead, cTail;
-        while(head <= tail) {
-            cHead = s.charAt(head);
-            cTail = s.charAt(tail);
-            if (!Character.isLetterOrDigit(cHead)) {
+        if(s == null || s.isEmpty()) return true;
+        
+        int head = 0;
+        int tail = s.length() - 1;
+        while(head <= tail){
+            char h = s.charAt(head);
+            char t = s.charAt(tail);
+            
+            if(!Character.isLetterOrDigit(h)){
                 head++;
-            } else if(!Character.isLetterOrDigit(cTail)) {
+            }else if(!Character.isLetterOrDigit(t)){
                 tail--;
-            } else {
-                if (Character.toLowerCase(cHead) != Character.toLowerCase(cTail)) {
-                    return false;
-                }
+            }else if(Character.toLowerCase(h) != Character.toLowerCase(t)){
+                return false;
+            }else{
                 head++;
                 tail--;
             }
         }
-
+        
         return true;
     }
 }
