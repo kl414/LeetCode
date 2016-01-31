ce21d9c7cd07a83419071172fffd21a83d0177e8
kl414 <kevinlin414@gmail.com>
2016-01-30 19:54:53 -0500

    Update Add_Binary.java

--- Add_Binary.java
+++ Add_Binary.java
@@ -1,29 +1,23 @@
 public class Solution {
     public String addBinary(String a, String b) {
-        if(a == null || a.isEmpty()) {
+        if(a == null || a.isEmpty()) 
             return b;
-        }
-        if(b == null || b.isEmpty()) {
+        if(b == null || b.isEmpty()) 
             return a;
-        }
-        char[] aArray = a.toCharArray();
-        char[] bArray = b.toCharArray();
-        StringBuilder stb = new StringBuilder();
 
-        int i = aArray.length - 1;
-        int j = bArray.length - 1;
-        int aByte;
-        int bByte;
+
+        int n = a.length() - 1;
+        int m = b.length() - 1;
+        StringBuilder res = new StringBuilder();
         int carry = 0;
-        int result;
-
-        while(i > -1 || j > -1 || carry == 1) {
-            aByte = (i > -1) ? Character.getNumericValue(aArray[i--]) : 0;
-            bByte = (j > -1) ? Character.getNumericValue(bArray[j--]) : 0;
-            result = aByte ^ bByte ^ carry;
-            carry = ((aByte + bByte + carry) >= 2) ? 1 : 0;
-            stb.append(result);
+        while(n >= 0 || m >= 0){
+            int sum = carry;
+            if(n >= 0) sum += a.charAt(n--) - '0';
+            if(m >= 0) sum += b.charAt(m--) - '0';
+            res.append(sum % 2);
+            carry = sum / 2;
         }
-        return stb.reverse().toString();
+        if(carry != 0) res.append("1");
+        return res.reverse().toString();
     }
 }
