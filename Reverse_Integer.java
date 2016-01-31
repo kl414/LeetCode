0cd8f045c876df0694ece2d84735ebd8e814dffd
kl414 <kevinlin414@gmail.com>
2016-01-30 20:16:03 -0500

    Update Reverse_Integer.java

--- Reverse_Integer.java
+++ Reverse_Integer.java
@@ -1,16 +1,22 @@
-public int reverse(int x)
-{
-    int result = 0;
-
-    while (x != 0)
-    {
-        int tail = x % 10;
-        int newResult = result * 10 + tail;
-        if ((newResult - tail) / 10 != result)
-        { return 0; }
-        result = newResult;
-        x = x / 10;
+/**
+ * for overflow checking, it can also be done by checking against
+ * INT_MAXVALUE & INT_MINVALUE
+ */
+public class Solution {
+    public int reverse(int x) {
+        int res = 0;
+        
+        while(x != 0){
+            int tail = x % 10;
+            int newRes = res * 10 + tail;
+            
+            //reverse the process to check if overflow
+            if( (newRes - tail) / 10 != res) return 0;
+            
+            res = newRes;
+            x /= 10;
+        }
+        
+        return res;
     }
-
-    return result;
 }
