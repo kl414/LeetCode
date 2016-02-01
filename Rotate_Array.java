dc543c45454be6292dee3ccc85771d829e13e910
kl414 <kevinlin414@gmail.com>
2016-01-31 20:10:58 -0500

    Update Rotate_Array.java

--- Rotate_Array.java
+++ Rotate_Array.java
@@ -1,16 +1,53 @@
-public void rotate(int[] nums, int k) {
-    k %= nums.length;
-    reverse(nums, 0, nums.length - 1);
-    reverse(nums, 0, k - 1);
-    reverse(nums, k, nums.length - 1);
-}
-
-public void reverse(int[] nums, int start, int end) {
-    while (start < end) {
-        int temp = nums[start];
-        nums[start] = nums[end];
-        nums[end] = temp;
-        start++;
-        end--;
+/**
+ * first calculate the steps for rotation
+ * then reverse the whole array, and split-reverse the two parts
+ */
+public class Solution {
+    public void rotate(int[] nums, int k) {
+        k %= nums.length;
+        reverse(nums, 0, nums.length - 1);
+        reverse(nums, 0, k - 1);
+        reverse(nums, k, nums.length - 1);
+    }
+    
+    public void reverse(int[] nums, int start, int end) {
+        while (start < end) {
+            //can also use something like this for swapping
+            /*
+            nums[n] ^= nums[m];
+            nums[m] ^= nums[n];
+            nums[n] ^= nums[m];
+            */
+            int temp = nums[start];
+            nums[start] = nums[end];
+            nums[end] = temp;
+            start++;
+            end--;
+        }
     }
 }
+
+//normal way
+/* 
+public class Solution {
+    public void rotate(int[] nums, int k) {
+        if(nums.length <= 1){
+            return;
+        }
+        //step each time to move
+        int step = k % nums.length;
+        int[] tmp = new int[step];
+        for(int i = 0; i < step; i++){
+            tmp[i] = nums[nums.length - step + i];
+        }
+        for(int i = nums.length - step - 1; i >= 0; i--){
+            nums[i + step] = nums[i];
+        }
+        for(int i = 0; i < step; i++){
+            nums[i] = tmp[i];
+        }
+
+    }
+
+}
+*/
