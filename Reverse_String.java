334c2470ff74fe2e5bbd442c40f3dddce9e6dc9f
kl414 <kevinlin414@gmail.com>
2016-04-22 16:13:04 -0400

    Reverse String 344

---
+++ Reverse_String.java
@@ -0,0 +1,17 @@
+public class Solution {
+    public String reverseString(String s) {
+        //will do it using charArray
+        //can also be done by recursion
+        char[] word = s.toCharArray();
+        int i = 0;
+        int j = s.length() - 1;
+        while (i < j) {
+            char temp = word[i];
+            word[i] = word[j];
+            word[j] = temp;
+            i++;
+            j--;
+        }
+        return new String(word);
+    }
+}
